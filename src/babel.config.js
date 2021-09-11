98fbd8b398e559e557379e4d4203928e335dcfd3
mikejguo290 <mikejguo290@gmail.com>
2021-09-11 13:10:50 +0100

    add babel packages to enable node to run jest with react and ES6 import etc.

--- src/babel.config.js
+++ src/babel.config.js
@@ -1,6 +1,16 @@
 module.exports = function(api) {
-    api.cache(true);
-    return {
-      presets: ['@babel/preset-env', '@babel/preset-react'],
-    };
+  api.cache(true);
+  return {
+    presets: ['@babel/preset-env', '@babel/preset-react'],
   };
+};
+
+
+/*
+module.exports = {
+    presets: [
+      ['@babel/preset-env', {targets: {node: 'current'}}],
+      ['@babel/preset-react', {targets: {node: 'current'}}] // add this
+    ]
+  };
+*/
