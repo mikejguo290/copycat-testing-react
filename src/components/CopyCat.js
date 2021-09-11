44bb3daec5ef20a4fab9324f2c434c7716c21a84
mikejguo290 <mikejguo290@gmail.com>
2021-09-11 02:05:45 +0100

    add comments to CopyCat to aid understanding.

--- src/components/CopyCat.js
+++ src/components/CopyCat.js
@@ -8,6 +8,10 @@ const images = {
 };
 
 export const CopyCat = ({name, value, handleChange, isCopying, toggleTape}) => {
+    // copyCat is a controlled component. it takes a prop. it displays a heading, input and an image. 
+    // isCopying - a state to record whether cat is Copying cat or not.
+    // toggleTape - a function to flip the isCopying state (after 0.5 seconds)
+    // handleChange - updates input in CopyCatContainer.js
   return (
     <div style={styles.divStyles}>
       <h1 style={{ marginBottom: 80 }}>Copy Cat {name || 'Tom'}</h1>
