b9c13e2e2aa7287c02fb96b96879ff8f7027c391
mikejguo290 <mikejguo290@gmail.com>
2021-09-11 18:06:41 +0100

    9. test app render user input below cat image whenever user types.

--- src/containers/__tests__/CopyCatContainer.test.js
+++ src/containers/__tests__/CopyCatContainer.test.js
@@ -5,11 +5,24 @@ import 'regenerator-runtime'
 import React from 'react';
 import { screen, render } from '@testing-library/react';
 import '@testing-library/jest-dom'; // get DOM specific matcher methods from jest-dom module
-import { userEvent } from '@testing-library/user-event'; // get userEvent object to mimick user interactions
+import  userEvent from '@testing-library/user-event'; // get userEvent object to mimick user interactions
 
 test('Should display copied text', () => {
 // Write your solution to task 9 within this test
+// test app render user input below cat image whenever user types.
+    render(<CopyCatContainer />); //parent will contain all elements rendered in child - CopyCat
+    // extract input
+    const input = screen.getByRole('textbox');
+    // simulate userEvent - typing into input.
+    userEvent.type(input, 'Hello World!');
+    // extract the paragraph below image 
+    const paragraph = screen.getByText('Hello World!'); 
+    // Q. queryByText also works. why not use queryByX which seems more flexible all the time? 
+    // A. In case the paragraph is not there, expect(null).toBeInTheDocument();  would not work! //received value must be an HTMLElement or an SVGElement.
+    // lesson is query has to sync with jest-dom matcher
 
+    // test expectation
+    expect(paragraph).toBeInTheDocument();
 
 })
 
